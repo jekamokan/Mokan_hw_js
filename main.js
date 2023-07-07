dd60f18b203146e70f8b5ba9c23743d16815354e
Yevhen Mokan <jekamokan1994@gmail.com>
2023-07-07 22:58:36 +0200

    1st part hw

---
+++ main.js
@@ -0,0 +1,87 @@
+// const value = prompt('Введіть число');
+// const newValue = +value;
+// if(newValue){
+//     alert(`Ви ввели ${value}`);
+// } else{
+//     alert("Введіть тільки числа");
+// }
+
+
+
+
+// const text = prompt('Введіть будь який текст(ЗАБОРОНЕНІ СЛОВА!!!: карамболь, струмінь)');
+
+// if(text.includes('карамболь', 'струмінь')){
+//     alert('Ви використали заборонені слова')
+// } else{
+//     alert(text);
+// }
+
+
+
+
+// const age = confirm('Вам є 18?');
+// console.log(`Вам є 18? ${age}`);
+// const country = confirm('Ви були у Новій Зеландії?');
+// console.log(`Ви були у Новій Зеландії? ${country}`);
+// const pet = confirm('Чи є у Вас домашня тварина?');
+// console.log(`Чи є у Вас домашня тварина? ${pet}`);
+
+
+
+
+
+
+// const age = confirm('Вам є 18?');
+// if(age){
+//     alert('Ви повнолітній, я Вас вітаю)')
+// } else{
+//     alert('Вам треба трошки підрости.')
+// }
+// const country = confirm('Ви були у Новій Зеландії?');
+// if(country){
+//     alert('І шо там? гарно?)))')
+// } else{
+//     alert('Не гайте час,купуйте швиденько квиток.')
+// }
+// const pet = confirm('Чи є у Вас домашня тварина?');
+// if(pet){
+//     alert('Класс))) я теж обожнюю тваринок.')
+// } else{
+//     alert('Шкода що немає(')
+// }
+
+
+
+// const curentSizeThings = +prompt('Write your size to convert in USA size. From 40 to 54');
+
+// if(curentSizeThings < 40){
+//     alert('Not enough, but you can choose size 6(S)')
+// } else if(curentSizeThings <= 42){
+//     alert('Your size 8(M)')
+// }
+//  else if(curentSizeThings <= 44){
+//     alert('Your size 10')
+// }
+//  else if(curentSizeThings <= 46){
+//     alert('Your size 12(L)')
+// }
+//  else if(curentSizeThings <= 48){
+//     alert('Your size 14')
+// }
+//  else if(curentSizeThings <= 50){
+//     alert('Your size 16(XL)')
+// }
+//  else if(curentSizeThings <= 52){
+//     alert('Your size 18')
+// }
+//  else if(curentSizeThings <= 54){
+//     alert('Your size 20(XXL)')
+// } else{
+//     alert('To much, but you can choose size 20(XXL)')
+// }
+ 
+
+
+// const sex = confirm('Are you man?');
+// alert(sex ? 'Hi is a man' : 'She is a women');
