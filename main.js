199a048b735126f0b1323e269ba3b460818c0e80
Yevhen Mokan <jekamokan1994@gmail.com>
2023-07-13 23:54:33 +0200

    new hw

--- main.js
+++ main.js
@@ -148,3 +148,186 @@
 // const secondOne = newArr.shift();
 // secondArr.unshift(secondOne, secondTwo, secondThree, secondFour, secondFive);
 // console.log(secondArr);
+
+
+
+// Copy
+
+// const arrTable = [
+//         [0, 0, 0, 0, 0],
+//         [0, 1, 2, 3, 4, 5], 
+//         [0, 2, 4, 6, 8, 10], 
+//         [0, 3, 6, 9, 12, 15], 
+//         [0, 4, 8, 12, 16, 20], 
+//         [0, 5, 10, 15, 20, 25]
+//     ]
+// const newArr = [arrTable.slice()];
+// console.log(newArr);
+
+
+
+
+// Deep Copy
+
+// const arrTable = [
+//         [0, 0, 0, 0, 0],
+//         [0, 1, 2, 3, 4, 5], 
+//         [0, 2, 4, 6, 8, 10], 
+//         [0, 3, 6, 9, 12, 15], 
+//         [0, 4, 8, 12, 16, 20], 
+//         [0, 5, 10, 15, 20, 25]
+//     ]
+// const newArr = [...arrTable];
+// console.log(newArr);
+
+
+
+// Array Equals
+
+// const arr1 = ['a', 'b', 'c'];
+// const arr2 = arr1;
+// alert(arr2 === arr1);
+
+
+
+// Flat
+
+// const arrTable = [
+//         [ 1, 2, 3, 4, 5], 
+//         [ 2, 4, 6, 8, 10], 
+//         [ 3, 6, 9, 12, 15], 
+//         [ 4, 8, 12, 16, 20], 
+//         [ 5, 10, 15, 20, 25]
+//     ]
+//     const newArr = [...arrTable[0], ...arrTable[1],  ...arrTable[2], ...arrTable[3], ...arrTable[4]]
+//     console.log(newArr);
+
+
+
+// Destruct
+// const text = prompt('Напишіть будь яке речення');
+// let[a] = text.slice(0, 1);
+// let[b] = text.slice(5, 6);
+// let[c] = text.slice(9, 10);
+// alert(`Перша буква "${a}"  п'ята буква "${b}"  дев'ята буква "${c}"`);
+
+
+// Destruct default
+
+// const text = prompt('Напишіть будь яке речення');
+// const [, secondLetter = '!', , fourthLetter = '!', fifthLetter ='!'] = text
+// alert(`Друга буква "${secondLetter}"  четверта буква "${fourthLetter}"  п'ята буква "${fifthLetter}"`);
+
+
+// Multiply table rest
+
+// const arrTable = [
+//         [0, 0, 0, 0, 0],
+//         [0, 1, 2, 3, 4, 5], 
+//         [0, 2, 4, 6, 8, 10], 
+//         [0, 3, 6, 9, 12, 15], 
+//         [0, 4, 8, 12, 16, 20], 
+//         [0, 5, 10, 15, 20, 25]
+//     ]
+// const[a, [b, ...one], [c, ...two], [d, ...three], [e, ...four], [f, ...five]] = arrTable
+// const newArr = [];
+// newArr.push(one, two, three, four, five);
+// console.log(newArr);
+
+
+// For Alert
+
+// const names = ["John", "Paul", "George", "Ringo"];
+// for(const name of names){
+//     alert(name)
+// }
+
+
+
+// For Select Option
+
+// const currencies = ["USD", "EUR", "GBP", "UAH"]
+// let str = "<select>"
+// for (const currency of currencies){
+// str+= `<option>${currency}</option>`
+// }
+// str+= "</select>"
+// document.write(str) 
+
+
+
+
+// For Table Horizontal
+
+// const names = ["John", "Paul", "George", "Ringo"]
+// let   str = "<table>"
+// for (const name of names){
+// str+= `<td>${name}</td>`
+// }
+// str+= "</table>"
+// document.write(str) 
+
+
+
+
+// For Table Vertical
+
+// const names = ["John", "Paul", "George", "Ringo"]
+// let   str = "<table>"
+// for (const name of names){
+// str+= `<tr><td>${name}</td></tr>`
+// }
+// str+= "</table>"
+// document.write(str)
+
+
+
+
+// For Table Letters
+
+// const currencies = ["USD", "EUR", "GBP", "UAH"]
+// let   str = "<table>"
+// for (const currency of currencies){ //цикл створює рядки
+//      // Одна ітерація циклу створює ОДНИЙ РЯДОК
+     
+//      str+=`<tr></tr>`
+//     console.log(currency)
+//     for (const letter of currency){ //цикл створює осередки в одному рядку
+//          //одна ітерація циклу створює ОДИН осередок
+//          str+= `<td>${letter}</td>`
+//         console.log(letter)
+//     }
+// }
+// str+= "</table>"
+// document.write(str) //document.write відобразить ваш HTML на сторінці
+
+
+
+
+// For Multiply Table
+
+// const arrTable = [
+//             [0, 0, 0, 0, 0],
+//             [0, 1, 2, 3, 4], 
+//             [0, 2, 4, 6, 8], 
+//             [0, 3, 6, 9, 12], 
+//             [0, 4, 8, 12, 16], 
+//         ];
+// let str = "<table>";
+// for(const arr of arrTable){
+//     str+= `<tr></tr>`;
+//     for(const value of arr){
+//         str+= `<td>${value}</td>`
+//     }
+// }
+// str+= "</table>";
+// document.write(str);
+
+
+
+
+// Function Capitalize
+
+// let str = "cANBerRa"
+// let result
+// console.log(result) //Canberra
